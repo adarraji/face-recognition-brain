ba606f003320d0beb65dcb91b5809fba8a452632
adarraji <a.darraji@outlook.com>
2022-07-01 19:35:58 -0400

    Added logo code and styles

--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -1,9 +1,17 @@
 import React from "react";
+import brain from "./brain.png";
+import "./Logo.css";
+import Tilty from 'react-tilty';
+
 
 const Logo = () => {
     return (
-        <div>
-
+        <div className="ma4 mt0">
+            <Tilty className="tilt br2 shadow-3" reverse axis="x" scale={1.2} perspective={900} max="66">
+                <div className="pa2">
+                    <img className="pa3" src={brain} alt="logo" />
+                </div>
+            </Tilty>
         </div>
     );
 };
