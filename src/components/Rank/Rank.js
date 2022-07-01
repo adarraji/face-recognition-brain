6fe3b48a294cc68c90acb8d484219e61f9ef51d4
adarraji <a.darraji@outlook.com>
2022-07-01 19:40:26 -0400

    Added Rank code

--- src/components/Rank/Rank.js
+++ src/components/Rank/Rank.js
@@ -3,7 +3,12 @@ import React from "react";
 const Rank = () => {
     return (
         <div>
-
+            <div className="f4 white">
+                {"User1, your current rank is ..."}
+            </div>
+            <div className="f4 white">
+                {"#5"}
+            </div>
         </div>
     );
 };
