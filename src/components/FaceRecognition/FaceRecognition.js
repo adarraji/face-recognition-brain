3c7f784bb7d0223026ade124dc7b2b0428195213
adarraji <a.darraji@outlook.com>
2022-07-01 19:59:20 -0400

    Added input image with test src

--- src/components/FaceRecognition/FaceRecognition.js
+++ src/components/FaceRecognition/FaceRecognition.js
@@ -1,9 +1,10 @@
 import React from "react";
+import "./FaceRecognition.css"
 
 const FaceRecognition = () => {
     return (
-        <div>
-
+        <div className="ma1">
+            <img className="inputImage pa2" alt="" src="https://upload.wikimedia.org/wikipedia/commons/thumb/3/35/Neckertal_20150527-6384.jpg/1280px-Neckertal_20150527-6384.jpg" />
         </div>
     );
 };
