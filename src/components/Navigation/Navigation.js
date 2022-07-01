0bb14ec9f441928eb9c15b21ce10751b94d0b3cf
adarraji <a.darraji@outlook.com>
2022-07-01 19:27:15 -0400

    Added code Nav signout

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -2,9 +2,9 @@ import React from "react";
 
 const Navigation = () => {
     return (
-        <div>
-
-        </div>
+        <nav>
+            <p className="flex justify-end f4 black underline link dim pointer">SignOut</p>
+        </nav>
     );
 };
 
