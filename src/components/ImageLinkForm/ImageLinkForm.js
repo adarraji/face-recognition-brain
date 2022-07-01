445589ef3e70496c54829d1d8c6abf8fafe73dda
adarraji <a.darraji@outlook.com>
2022-07-01 19:50:30 -0400

    Added Image Link Form

--- src/components/ImageLinkForm/ImageLinkForm.js
+++ src/components/ImageLinkForm/ImageLinkForm.js
@@ -1,11 +1,18 @@
 import React from "react";
+import "./ImageLinkForm.css"
 
-const ImageLinkForm = () => {
+const Rank = () => {
     return (
         <div>
-
+            <p>{"Enter an image link. This magic brain will detect faces in your pictures"}</p>
+            <div className="flex justify-center">
+                <div className="form br3 shadow-5 pa4 flex justify-center">
+                    <input className="f4 pa2 w-70" type="text" />
+                    <button className="f4 pv2 ph3 w-30 white bg-light-purple link grow pointer">Detect</button>
+                </div>
+            </div>
         </div>
     );
 };
 
-export default ImageLinkForm;
+export default Rank;
