3c952ca25f787bc3e8dcf878f8f1e2226a311e80
techbrain2 <techbrain203@outlook.com>
2022-07-03 15:14:21 -0400

    updated styles

--- src/components/SignIn/SignIn.js
+++ src/components/SignIn/SignIn.js
@@ -20,7 +20,7 @@ const SignIn = ({ onRouteChnage }) => {
                         <input onClick={() => onRouteChnage("home")} className="b ph3 pv2 input-reset ba b--black bg-transparent grow pointer f6 dib" type="submit" value="Sign in" />
                     </div>
                     <div className="lh-copy mt3">
-                        <p onClick={() => onRouteChnage("register")} className="f6 link dim black db">Register</p>
+                        <p onClick={() => onRouteChnage("register")} className="f6 link dim black db pointer">Register</p>
                     </div>
                 </div>
             </main>
