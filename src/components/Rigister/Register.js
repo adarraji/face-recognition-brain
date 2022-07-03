217017bac769d0803fbdbe72e22a31b75a29ec83
techbrain2 <techbrain203@outlook.com>
2022-07-03 15:15:17 -0400

    Removed second button

--- src/components/Rigister/Register.js
+++ src/components/Rigister/Register.js
@@ -19,9 +19,6 @@ const Register = ({ onRouteChnage }) => {
                     <div className="">
                         <input onClick={() => onRouteChnage("home")} className="b ph3 pv2 input-reset ba b--black bg-transparent grow pointer f6 dib" type="submit" value="Register" />
                     </div>
-                    <div className="lh-copy mt3">
-                        <a href="#0" className="f6 link dim black db">Register</a>
-                    </div>
                 </div>
             </main>
         </article>
