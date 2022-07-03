ba73c82155dbfa034b6f0eda6be762f026ef4c05
techbrain2 <techbrain203@outlook.com>
2022-07-03 15:13:57 -0400

    Added route to register form

--- src/components/SignIn/SignIn.js
+++ src/components/SignIn/SignIn.js
@@ -20,7 +20,7 @@ const SignIn = ({ onRouteChnage }) => {
                         <input onClick={() => onRouteChnage("home")} className="b ph3 pv2 input-reset ba b--black bg-transparent grow pointer f6 dib" type="submit" value="Sign in" />
                     </div>
                     <div className="lh-copy mt3">
-                        <a href="#0" className="f6 link dim black db">Register</a>
+                        <p onClick={() => onRouteChnage("register")} className="f6 link dim black db">Register</p>
                     </div>
                 </div>
             </main>
