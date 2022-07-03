6e83b3dbe85453d72bd892b721554f6c2d2e503d
techbrain2 <techbrain203@outlook.com>
2022-07-03 15:17:08 -0400

    Added name field to register

--- src/components/Rigister/Register.js
+++ src/components/Rigister/Register.js
@@ -8,8 +8,12 @@ const Register = ({ onRouteChnage }) => {
                     <fieldset id="sign_up" className="ba b--transparent ph0 mh0">
                         <legend className="f1 fw6 ph0 mh0">Register</legend>
                         <div className="mt3">
+                            <label className="db fw6 lh-copy f6" htmlFor="email-address">Name</label>
+                            <input className="pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100" type="text" name="name" id="name" />
+                        </div>
+                        <div className="mt3">
                             <label className="db fw6 lh-copy f6" htmlFor="email-address">Email</label>
-                            <input className="pa2 input-reset ba bg-transparent hover-bg-black h    over-white w-100" type="email" name="email-address" id="email-address" />
+                            <input className="pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100" type="email" name="email-address" id="email-address" />
                         </div>
                         <div className="mv3">
                             <label className="db fw6 lh-copy f6" htmlFor="password">Password</label>
