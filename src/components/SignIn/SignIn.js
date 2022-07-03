9025a105ac7fcc6e63428a0194ce7d7674fc1a34
techbrain2 <techbrain203@outlook.com>
2022-07-03 15:06:06 -0400

    chnage form tag  to div tag

--- src/components/SignIn/SignIn.js
+++ src/components/SignIn/SignIn.js
@@ -4,7 +4,7 @@ const SignIn = ({ onRouteChnage }) => {
     return (
         <article className="br2 shadow-5 ba b--black-10 mv4 w-100 w-50-m w-25-l mw6 center">
             <main className="pa4 black-80">
-                <form className="measure center">
+                <div className="measure center">
                     <fieldset id="sign_up" className="ba b--transparent ph0 mh0">
                         <legend className="f1 fw6 ph0 mh0">Sign In</legend>
                         <div className="mt3">
@@ -22,7 +22,7 @@ const SignIn = ({ onRouteChnage }) => {
                     <div className="lh-copy mt3">
                         <a href="#0" className="f6 link dim black db">Register</a>
                     </div>
-                </form>
+                </div>
             </main>
         </article>
     );
