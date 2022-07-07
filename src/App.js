6fcfbbd6ef6eb3fc41f47667b34dc41bd45e1095
adarraji <a.darraji@outlook.com>
2022-07-07 09:37:05 -0400

    removed componentDidMount

--- src/App.js
+++ src/App.js
@@ -25,12 +25,6 @@ class App extends Component {
     };
   }
 
-  componentDidMount() {
-    fetch("http://localhost:3000/")
-      .then(response => response.json())
-      .then(console.log);
-  }
-
   onRouteChnage = (route) => {
     if (route === "signout") {
       this.setState({ isSignedIn: false });
