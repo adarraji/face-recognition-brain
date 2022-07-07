81be619ce968d602ae343a3a10fa8198adc21edb
adarraji <a.darraji@outlook.com>
2022-07-07 14:18:39 -0400

    updated if statement because server now returns real user

--- src/components/SignIn/SignIn.js
+++ src/components/SignIn/SignIn.js
@@ -30,7 +30,7 @@ class SignIn extends React.Component {
         })
             .then(response => response.json())
             .then(data => {
-                if (data === "success") {
+                if (data) {
                     this.props.onRouteChnage("home");
                 }
             });
