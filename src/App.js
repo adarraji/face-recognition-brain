abaf503a65f2e696da10fa51d011c5a68003d12f
adarraji <a.darraji@outlook.com>
2022-07-07 09:24:09 -0400

    Added componentDidMount and test the server

--- src/App.js
+++ src/App.js
@@ -25,6 +25,12 @@ class App extends Component {
     };
   }
 
+  componentDidMount() {
+    fetch("http://localhost:3000/")
+      .then(response => response.json())
+      .then(console.log);
+  }
+
   onRouteChnage = (route) => {
     if (route === "signout") {
       this.setState({ isSignedIn: false });
