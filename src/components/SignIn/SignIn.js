3277b38ab5d2aa0ced6e3aedfe9882bca4916669
adarraji <a.darraji@outlook.com>
2022-07-07 09:41:34 -0400

    changed SignIn to class component

--- src/components/SignIn/SignIn.js
+++ src/components/SignIn/SignIn.js
@@ -1,31 +1,34 @@
 import React from "react";
 
-const SignIn = ({ onRouteChnage }) => {
-    return (
-        <article className="br2 shadow-5 ba b--black-10 mv4 w-100 w-50-m w-25-l mw6 center">
-            <main className="pa4 black-80">
-                <div className="measure center">
-                    <fieldset id="sign_up" className="ba b--transparent ph0 mh0">
-                        <legend className="f1 fw6 ph0 mh0">Sign In</legend>
-                        <div className="mt3">
-                            <label className="db fw6 lh-copy f6" htmlFor="email-address">Email</label>
-                            <input className="pa2 input-reset ba bg-transparent hover-bg-black h    over-white w-100" type="email" name="email-address" id="email-address" />
+class SignIn extends React.Component {
+    render() {
+        const { onRouteChnage } = this.props;
+        return (
+            <article className="br2 shadow-5 ba b--black-10 mv4 w-100 w-50-m w-25-l mw6 center">
+                <main className="pa4 black-80">
+                    <div className="measure center">
+                        <fieldset id="sign_up" className="ba b--transparent ph0 mh0">
+                            <legend className="f1 fw6 ph0 mh0">Sign In</legend>
+                            <div className="mt3">
+                                <label className="db fw6 lh-copy f6" htmlFor="email-address">Email</label>
+                                <input className="pa2 input-reset ba bg-transparent hover-bg-black h    over-white w-100" type="email" name="email-address" id="email-address" />
+                            </div>
+                            <div className="mv3">
+                                <label className="db fw6 lh-copy f6" htmlFor="password">Password</label>
+                                <input className="b pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100" type="password" name="password" id="password" />
+                            </div>
+                        </fieldset>
+                        <div className="">
+                            <input onClick={() => onRouteChnage("home")} className="b ph3 pv2 input-reset ba b--black bg-transparent grow pointer f6 dib" type="submit" value="Sign in" />
                         </div>
-                        <div className="mv3">
-                            <label className="db fw6 lh-copy f6" htmlFor="password">Password</label>
-                            <input className="b pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100" type="password" name="password" id="password" />
+                        <div className="lh-copy mt3">
+                            <p onClick={() => onRouteChnage("register")} className="f6 link dim black db pointer">Register</p>
                         </div>
-                    </fieldset>
-                    <div className="">
-                        <input onClick={() => onRouteChnage("home")} className="b ph3 pv2 input-reset ba b--black bg-transparent grow pointer f6 dib" type="submit" value="Sign in" />
                     </div>
-                    <div className="lh-copy mt3">
-                        <p onClick={() => onRouteChnage("register")} className="f6 link dim black db pointer">Register</p>
-                    </div>
-                </div>
-            </main>
-        </article>
-    );
+                </main>
+            </article>
+        );
+    }
 };
 
 export default SignIn;
