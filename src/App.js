dd9538e6cbcaadca26661a7194e6083b2b290499
adarraji <a.darraji@outlook.com>
2022-07-21 19:43:24 -0400

    added catch to image fetch

--- src/App.js
+++ src/App.js
@@ -90,6 +90,7 @@ class App extends Component {
           })
             .then(response => response.json())
             .then(count => this.setState(Object.assign(this.state.user, { entries: count })))
+            .catch(console.log);
         }
         this.displayfaceBox(this.calculateFaceLocation(response))
       })
