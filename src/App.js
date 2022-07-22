b61e3429d5ac7e7bbd56ae58b2b30ec2f4601727
adarraji <a.darraji@outlook.com>
2022-07-22 17:18:11 -0400

    moved clarifai api key to backend

--- src/App.js
+++ src/App.js
@@ -78,7 +78,14 @@ class App extends Component {
 
   onButtonSubmit = () => {
     this.setState({ imageURL: this.state.input });
-    app.models.predict(Clarifai.FACE_DETECT_MODEL, this.state.input)
+    fetch("http://localhost:3000/imageurl", {
+      method: "post",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({
+        input: this.state.input,
+      })
+    })
+      .then(response => response.json())
       .then(response => {
         if (response) {
           fetch("http://localhost:3000/image", {
