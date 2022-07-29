7d07a478780a28efe7f505598a603eb83b81a135
adarraji <a.darraji@outlook.com>
2022-07-29 15:57:41 -0400

    installed serve

--- src/App.js
+++ src/App.js
@@ -88,7 +88,7 @@ class App extends Component {
       .then(response => response.json())
       .then(response => {
         if (response) {
-          fetch("https://mighty-citadel-11675.herokuapp.com:3000/image", {
+          fetch("https://mighty-citadel-11675.herokuapp.com/image", {
             method: "put",
             headers: { "Content-Type": "application/json" },
             body: JSON.stringify({
